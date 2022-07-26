03d294918b092c8ffbf7899c12deae12b0b61edf
Dave <david-rowe2@live.nmit.ac.nz>
2022-07-27 10:43:39 +1200

    listening on port 3000

---
+++ _node/app.js
@@ -0,0 +1,19 @@
+//ANCHOR To run "node app.js" in bash terminal
+
+//create a variable named express that has a value of the express library
+const express = require('express');
+
+// asign variable app as a function
+const app = express();
+
+//calling app function using method with http url response and request parameters in es6 which returns a response with the send method parameter that contains the string "hello world"
+app.get("/", (req, res) => {
+    
+    res.send("hello world");
+});
+
+//calling app function using the method listening with a parameter of 3000 and prints a string 
+//listening on the local host 3000 port
+app.listen(3000, () => {
+    console.log("started on port 3000.")
+});
